5e528539369c1c9f318e76cf419146b8d215ff29
wugo2021 <79857285+wugo2021@users.noreply.github.com>
2022-04-29 21:52:15 +0200

    Update run-huawei-aonly.sh

--- run-huawei-aonly.sh
+++ run-huawei-aonly.sh
@@ -394,14 +394,17 @@ mount -o loop,rw s-aonly.img d
     	sed -i "/ro.product.model/d" etc/prop.default
     	sed -i "/ro.product.system.model/d" etc/prop.default
         sed -i "/ro.product.name/d" etc/prop.default
-        sed -i "ro.build.display.id/d" build.prop
+        sed -i "ro.build.display.id/d" etc/prop.default
+	sed -i "ro.build.display.id/d" build.prop
         #sed -i "ro.product.locale=en-US/d" build.prop
 	
     	echo "ro.product.manufacturer=HUAWEI" >> etc/prop.default
     	echo "ro.product.system.model=hi6250" >> etc/prop.default
     	echo "ro.product.model=$model" >> etc/prop.default
         echo "ro.product.name=$model" >> etc/prop.default
-        echo "ro.build.display.id=RQ3A.211001.001.$data" >> build.prop
+        echo "ro.build.display.id=$data" >> etc/prop.default
+	echo "ro.build.display.id=$data" >> build.prop
+        
 
     	#VERSION="LeaOS"
     	#VERSION="crDRom v316 - Mod Iceows"
