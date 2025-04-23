41fadd6535d661430f97357774b5158b0d05994a
wugo2021 <79857285+wugo2021@users.noreply.github.com>
2025-04-23 22:51:04 +0200

    Update run-huawei-emui9-ab-a14.sh

--- run-huawei-emui9-ab-a14.sh
+++ run-huawei-emui9-ab-a14.sh
@@ -43,7 +43,9 @@ mount -o loop,rw s-ab-raw.img d
 	rm -rf splash2
 	rm -rf modem_log
 	rm -rf preavs
-
+       #有病毒
+       rm -fr system/priv-app/LiveWallpapersPicker
+       rm -fr system/priv-app/Shell
 
  
 	mkdir splash2
@@ -473,9 +475,7 @@ mount -o loop,rw s-ab-raw.img d
 	rm -rf product/overlay/treble-overlay-razer-*
 	rm -rf product/overlay/treble-overlay-sharp-*
 	
-       #有病毒
-       rm -fr sysmain/system/priv-app/LiveWallpapersPicker
-       rm -fr sysmain/system/priv-app/Shell
+
 
 	# Tee Deamon
 	cp "$origin/files-patch/system/bin/tee_auth_daemon" bin/tee_auth_daemon
