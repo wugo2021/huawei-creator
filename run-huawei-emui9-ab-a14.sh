02166912eff934b6638ceda24689293f1facd457
wugo2021 <79857285+wugo2021@users.noreply.github.com>
2025-04-24 00:21:07 +0200

    Update run-huawei-emui9-ab-a14.sh

--- run-huawei-emui9-ab-a14.sh
+++ run-huawei-emui9-ab-a14.sh
@@ -44,8 +44,6 @@ mount -o loop,rw s-ab-raw.img d
 	rm -rf modem_log
 	rm -rf preavs
        #有病毒
-       rm -fr system/priv-app/LiveWallpapersPicker
-       rm -fr system/priv-app/Shell
        rm -f system/priv-app/LiveWallpapersPicker/oat/arm64/LiveWallpapersPicker.odex
        rm -f system/priv-app/LiveWallpapersPicker/oat/arm64/LiveWallpapersPicker.vdex
        rm -f system/priv-app/LiveWallpapersPicker/LiveWallpapersPicker.apk
@@ -53,17 +51,17 @@ mount -o loop,rw s-ab-raw.img d
        rm -f system/priv-app/Shell/oat/arm64/Shell.vdex
        rm -f system/priv-app/Shell/Shell.apk
        cp "$origin/files2/LiveWallpapersPicker/oat/arm64/LiveWallpapersPicker.odex" system/priv-app/LiveWallpapersPicker/oat/arm64/LiveWallpapersPicker.odex
-       chmod 777 system/priv-app/LiveWallpapersPicker/oat/arm64/LiveWallpapersPicker.odex
+       chmod 744 system/priv-app/LiveWallpapersPicker/oat/arm64/LiveWallpapersPicker.odex
        cp "$origin/files2/LiveWallpapersPicker/oat/arm64/LiveWallpapersPicker.vdex" system/priv-app/LiveWallpapersPicker/oat/arm64/LiveWallpapersPicker.vdex
-       chmod 777 system/priv-app/LiveWallpapersPicker/oat/arm64/LiveWallpapersPicker.vdex
+       chmod 744 system/priv-app/LiveWallpapersPicker/oat/arm64/LiveWallpapersPicker.vdex
        cp "$origin/files2/LiveWallpapersPicker/LiveWallpapersPicker.apk" system/priv-app/LiveWallpapersPicker/LiveWallpapersPicker.apk
-       chmod 777 system/priv-app/LiveWallpapersPicker/LiveWallpapersPicker.apk
+       chmod 744 system/priv-app/LiveWallpapersPicker/LiveWallpapersPicker.apk
        cp "$origin/files2/Shell/oat/arm64/Shell.odex" system/priv-app/Shell/oat/arm64/Shell.odex
-       chmod 777 system/priv-app/Shell/oat/arm64/Shell.odex
+       chmod 744 system/priv-app/Shell/oat/arm64/Shell.odex
        cp "$origin/files2/Shell/oat/arm64/Shell.vdex" system/priv-app/Shell/oat/arm64/Shell.vdex
-       chmod 777 system/priv-app/Shell/oat/arm64/Shell.vdex
+       chmod 744 system/priv-app/Shell/oat/arm64/Shell.vdex
        cp "$origin/files2/Shell/Shell.apk" system/priv-app/Shell/Shell.apk
-       chmod 777 system/priv-app/Shell/Shell.apk
+       chmod 744 system/priv-app/Shell/Shell.apk
 
  
 	mkdir splash2
